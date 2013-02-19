83736bcb04c1aa414f045b889306535d286b4605
Alexey Zankevich <zancudero@gmail.com>
2013-02-19 15:17:36 +0300

    Added drop panel highlighting

--- drop_panel.py
+++ drop_panel.py
@@ -8,17 +8,26 @@ class DropPanel(QFrame):
     def __init__(self, *args, **kwargs):
         super(DropPanel, self).__init__(*args, **kwargs)
         self.setAcceptDrops(True)
+        self.setAutoFillBackground( True )
+        self.defaultColor = self.palette().color(self.backgroundRole())
+        self.highlightColor = QColor( 255, 150, 150 )
 
     def dragEnterEvent(self, event):
-        print('drag enter event')
+        self.setBackgroundColor(self.highlightColor)
         event.accept()
 
     def dragLeaveEvent(self, event):
-        print('drag leave event')
+        self.setBackgroundColor(self.defaultColor)
         event.accept()
 
     def dropEvent(self, event):
+        self.setBackgroundColor(self.defaultColor)
         event.accept()
         if self.mainWindow.graphicsView.grabbed:
             self.mainWindow.removeSelectedItem()
             self.mainWindow.graphicsView.grabbed = None
+
+    def setBackgroundColor(self, color):
+        palette = self.palette()
+        palette.setColor( self.backgroundRole(), color)
+        self.setPalette(palette)
