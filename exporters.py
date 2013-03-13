adffeec6b97f14768f695c2ff4bdca1d9cf9abb4
Alexey Zankevich <zancudero@gmail.com>
2013-03-13 11:11:00 +0300

    Changed lua exporter

--- exporters.py
+++ exporters.py
@@ -4,26 +4,36 @@ from StringIO import StringIO
 __author__ = 'alexey.zankevich'
 
 
+import const
+
+
 class LuaExporter(object):
     def export(self, d):
         out = StringIO()
         out.write("return {\n")
+        indent = 4
+        for key in [const.KEY_LAYOUT_TYPE, const.KEY_LAYOUT_ANCHOR_X, const.KEY_LAYOUT_ANCHOR_Y, const.KEY_LAYOUT_WIDTH,
+                    const.KEY_LAYOUT_HEIGHT]:
+            if key in d:
+                out.write(self.formatKeyValue(key, d[key], indent))
+        out.write("%s[\"props\"] = {\n" %(indent * " "))
         props = d[const.KEY_PROPS]
         for i, prop in enumerate(props):
-            out.write(self.formatProp(prop))
+            out.write(self.formatProp(prop, indent + 4))
             if i + 1 < len(props):
                 out.write(",\n")
             else:
                 out.write("\n")
+        out.write("%s}\n" %(indent * " "))
         out.write("}")
         out.seek(0)
         return out.read()
 
-    def formatProp(self, prop):
-        s = "    {\n"
+    def formatProp(self, prop, indent):
+        s = "%s{\n" %(indent * " ")
         for key, value in prop.items():
-            s += self.formatKeyValue(key, value)
-        s += "    }"
+            s += self.formatKeyValue(key, value, indent+4)
+        s += "%s}" %(indent * " ")
         return s
 
     def formatBool(self, b):
@@ -32,11 +42,11 @@ class LuaExporter(object):
     def formatString(self, s):
         return '"%s"' %s
 
-    def formatKeyValue(self, key, value):
+    def formatKeyValue(self, key, value, indent):
         if type(value) in (int, float):
-            return "        [\"%s\"] = %s,\n" %(key, value)
+            return "%s[\"%s\"] = %s,\n" %(indent * " ", key, value)
         if type(value) == bool:
-            return "        [\"%s\"] = %s,\n" %(key, self.formatBool(value))
+            return "%s[\"%s\"] = %s,\n" %(indent * " ", key, self.formatBool(value))
         else:
-            return "        [\"%s\"] = %s,\n" %(key, self.formatString(value))
+            return "%s[\"%s\"] = %s,\n" %(indent * " ", key, self.formatString(value))
 
