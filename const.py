34c0698f11ac9ffa0e0c3316b796978d99373836
Alexey Zankevich <zancudero@gmail.com>
2013-04-23 16:33:30 +0300

    udpated designer ui

--- const.py
+++ const.py
@@ -3,14 +3,13 @@ __author__ = 'alex'
 #prop-related consts
 KEY_X = 'x'
 KEY_Y = 'y'
-KEY_X_UNIT = 'x_unit'
-KEY_Y_UNIT = 'y_unit'
-KEY_HORIZONTAL_ALIGN = 'h_align'
 KEY_NAME = 'name'
 KEY_TYPE = 'type'
 KEY_PROPS = 'props'
 KEY_ID = 'id'
 KEY_UID = 'uid'
+KEY_Z_INDEX = 'z_index'
+KEY_HORIZONTAL_ALIGN = 'h_align'
 KEY_WORKING_DIR = 'working_dir'
 KEY_LAYOUT_PATH = 'layout_path'
 KEY_LAYOUT_TYPE = 'layout_type'
@@ -23,13 +22,5 @@ SCALABLE_LAYOUT_TYPE = 'scalable'
 ALIGN_LEFT = 'left'
 ALIGN_RIGHT = 'right'
 ALIGN_CENTER = 'center'
-
-DEFAULT_LAYOUT_H_ALIGN = ALIGN_CENTER
-
-UNIT_PX = 'px'
-UNIT_PX_POS = 0
-
-UNIT_PERCENT = '%'
-UNIT_PERCENT_POS = 1
 
 PATH_NOT_SPECIFIED_TEXT = 'Not Specified'
